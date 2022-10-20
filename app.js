94bd016b7b42946cb8227a168014d99f28b323a3
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-10-20 20:02:54 +0900

    add authentication using username and password. And implement encryption of password using mongoose-encryption

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -10,6 +12,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+
+
+const User = new mongoose.model("User", userSchema);
 
 app.get('/', function(req, res){
     res.render('home');
@@ -21,6 +37,39 @@ app.get('/login', function(req, res){
 
 app.get('/register', function(req, res){
     res.render('register');
+});
+
+app.post('/register', function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    console.log(newUser.password + ',' + newUser.email);
+    newUser.save(function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+    
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    });
 });
 
 const port = 3000;
